f3b0658f0d87a00e7dd3808ee997c254263dabe6
Elizabeth <bagiraliza21@gmail.com>
2021-07-05 23:14:29 +0300

    deb

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,12 @@
 import { fetchCountries } from './service/api-service.js';
-import countryInfo from './tpl/country.handlebars';
+import countryInfo from './tpl/country.hbs';
 
 console.log(countryInfo);
 
-const input = document.querySelector('[name="country"]');
+const inputRef = document.querySelector('[name="country"]');
 const result = document.querySelector('.result');
 
-input.addEventListener('input', searchCountries);
+inputRef.addEventListener('input', _.debounce(searchCountries, 500));
 
 function searchCountries(e) {
   const searchWord = e.target.value.trim();
@@ -21,15 +21,15 @@ function searchCountries(e) {
 function renderCountries(countries) {
   if (countries.length === 1) {
     result.insertAdjacentHTML('afterbegin', countryInfo(countries));
-
-    // вывести подр информацию о стране с пом шабл hbs
   } else if (countries.length > 1 && countries.length <= 10) {
     const counrtyList = results.map(country => {
       return `<li><p>${country}</p></li>`;
     });
     result.insertAdjacentHTML('beforeend', counrtyList.join(''));
   } else if (countries.length > 10) {
-    // error
+    error({
+      text: 'Notice me, senpai!',
+    });
 
     console.log(countries);
   }
